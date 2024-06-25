761ae1cc03f09d39920174a4ccecffd7fdb2b0db
Thirani Imanya <153622537+Thirani15@users.noreply.github.com>
2024-06-25 08:44:12 +0530

    Add files via upload

---
+++ Practical 02/Q4.scala
@@ -0,0 +1,30 @@
+object Q4
+{
+  def attendees(price: Int): Int = 
+  {
+     120 + 4*(15 - price)
+  }
+
+  def revenue(price: Int): Int = 
+  {
+    attendees(price) * price
+  }
+
+  def cost(price: Int): Int = 
+  {
+    500+attendees(price)
+  }
+
+  def profit(price: Int): Int = 
+  {
+    revenue(price) - cost(price)
+  }
+    
+    
+  def main(args: Array[String]): Unit = 
+    {
+        print(profit(5), profit(10), profit(15), profit(20))
+        print(profit(25), profit(30), profit(35), profit(40))
+    }
+    
+}
