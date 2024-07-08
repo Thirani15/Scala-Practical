66995ac084ebf3f63fa046d6989ab5ccdd390f0e
Thirani <thiraniwork@gmail.com>
2024-07-09 00:48:31 +0530

    q3 updated

---
+++ Practical 04/Q3.scala
@@ -0,0 +1,20 @@
+object q3
+{
+  def toUpper(input: String): String = input.toUpperCase()
+
+  def toLower(input: String): String = input.toLowerCase()
+
+  def formatNames(name: String, formatFunction: String => String): String = formatFunction(name)
+
+  def main(args: Array[String]): Unit = 
+  {
+    val names = List("Benny", "Niroshan", "Saman", "Kumara")
+    val formattedNames = names.map{
+      case "Benny" => formatNames("Benny", toUpper)
+      case "Niroshan" => formatNames("Niroshan", name => name.substring(0, 2).toUpperCase + name.substring(2))
+      case "Saman" => formatNames("Saman", toLower)
+      case "Kumara" => formatNames("Kumara", name => name.substring(0, 1).toUpperCase + name.substring(1, 5).toLowerCase + name.substring(5).toUpperCase)
+    }
+    formattedNames.foreach(println)
+  }
+}
